829ca5a753410b1f49e5a2c41b9dfd6a3f2d445f
46kuri_ <ghost.46kuri@gmail.com>
2021-08-18 22:09:49 +0900

    Add files via upload

--- TheDesignationTpPL/src/main/java/thedesignationtppl/thedesignationtppl/TheDesignationTpPL.java
+++ TheDesignationTpPL/src/main/java/thedesignationtppl/thedesignationtppl/TheDesignationTpPL.java
@@ -29,57 +29,34 @@ public final class TheDesignationTpPL extends JavaPlugin implements Listener {
             if (args.length <= 0) {
                 return false;
             }
-            if (args[0].equalsIgnoreCase("set1")) {
-                Location loc = p.getLocation();
-                World world = loc.getWorld();
-                if (world == null) return false;
-                String name = world.getName();
-                int x = loc.getBlockX();
-                int y = loc.getBlockY();
-                int z = loc.getBlockZ();
-                int yaw = (int) loc.getYaw();
-                int pitch = (int) loc.getPitch();
-                getConfig().set("set1",name + "," + x + "," + y + "," + z + "," + yaw + "," + pitch);
-                saveConfig();
-                return true;
-            }
-            if (args[0].equalsIgnoreCase("set2")) {
-                Location loc = p.getLocation();
-                World world = loc.getWorld();
-                if (world == null) return false;
-                String name = world.getName();
-                int x = loc.getBlockX();
-                int y = loc.getBlockY();
-                int z = loc.getBlockZ();
-                int yaw = (int) loc.getYaw();
-                int pitch = (int) loc.getPitch();
-                getConfig().set("set2",name + "," + x + "," + y + "," + z + "," + yaw + "," + pitch);
-                saveConfig();
-                return true;
+            if (args[0].equalsIgnoreCase("set")) {
+                if (args.length <= 1) {
+                    return false;
+                }
+                String name0 = args[1];
+                if (name0 == null) return false;
+                if (args[1].equalsIgnoreCase(name0)){
+                    Location loc = p.getLocation();
+                    World world = loc.getWorld();
+                    if (world == null) return false;
+                    String name = world.getName();
+                    int x = loc.getBlockX();
+                    int y = loc.getBlockY();
+                    int z = loc.getBlockZ();
+                    int yaw = (int) loc.getYaw();
+                    int pitch = (int) loc.getPitch();
+                    getConfig().set(name0,name + "," + x + "," + y + "," + z + "," + yaw + "," + pitch);
+                    saveConfig();
+                    return true;
+                }
             }
             if (args[0].equalsIgnoreCase("warp")) {
                 if (args.length <= 1) {
                     return false;
                 }
-                if (args[1].equalsIgnoreCase("set1")) {
-                    String data = getConfig().getString(args[1]);
-                    if (data == null) return false;
-                    String[] loc = data.split(",");
-                    World world = Bukkit.getServer().getWorld(loc[0]);
-                    double x = Double.parseDouble(loc[1]);
-                    double y = Double.parseDouble(loc[2]);
-                    double z = Double.parseDouble(loc[3]);
-                    int yaw = (int) Double.parseDouble(loc[4]);
-                    int pitch = (int) Double.parseDouble(loc[5]);
-                    Location location = new Location(world, x, y, z);
-                    location.setPitch(pitch);
-                    location.setYaw(yaw);
-                    p.teleport(location);
-                    return true;
-                }
-                if (args[1].equalsIgnoreCase("set2")) {
-                    String data = getConfig().getString(args[1]);
-                    if (data == null) return false;
+                String data = getConfig().getString(args[1]);
+                if (data == null) return false;
+                if (args[1].equalsIgnoreCase(data)) {
                     String[] loc = data.split(",");
                     World world = Bukkit.getServer().getWorld(loc[0]);
                     double x = Double.parseDouble(loc[1]);
